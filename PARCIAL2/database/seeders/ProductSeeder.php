36c1422b9c675948a3a4602cf803fde4ffdedbd3
nataliaocmo <nataliaocmo@unisabana.edu.co>
2023-10-08 22:03:16 -0500

    q

--- PARCIAL2/database/seeders/ProductSeeder.php
+++ PARCIAL2/database/seeders/ProductSeeder.php
@@ -22,112 +22,128 @@ class ProductSeeder extends Seeder
            'name'=> "Pizza Peperoni",
            'price'=> 85000,
             'imagen'=> "PROYECTO-main\Comida\pizza_especialidad.jpg",
-           'category'=>"RECOMENDACION"
+           'category'=>"RECOMENDACION",
+           'quantity'=> 1000
        ]);
        \App\Models\Product::create([
            
            'name'=> "Bruscheta",
            'price'=> 28000,
            'imagen'=> "PROYECTO-main\Comida\Bruschettas.jpg",
-           'category'=>"ENTRADAS"
+           'category'=>"ENTRADAS",
+           'quantity'=> 1000
        ]);
        \App\Models\Product::create([
            
            'name'=> "Vuelta al Mundo en 80 quesos",
            'price'=> 40000,
            'imagen'=> "PROYECTO-main\Comida\Vulta_al_mundo_en_80_quesos.jpg",
-           'category'=>"ENTRADAS"
+           'category'=>"ENTRADAS",
+           'quantity'=> 1000
         ]);
         \App\Models\Product::create([
            
             'name'=> "Muffins de Pizza",
             'price'=> 35000,
             'imagen'=> "PROYECTO-main\Comida\Muffins de Pizza.jpg",
-            'category'=>"ENTRADAS"
+            'category'=>"ENTRADAS",
+            'quantity'=> 1000
         ]);
         \App\Models\Product::create([
             
             'name'=> "Pasta carbonara",
             'price'=> 70000,
             'imagen'=> "PROYECTO-main\Comida\Pasta_Carbonara.jpg",
-            'category'=>"PLATOS"
+            'category'=>"PLATOS",
+            'quantity'=> 1000
         ]);
         \App\Models\Product::create([
             
             'name'=> "Lasagna",
             'price'=> 60000,
             'imagen'=> "PROYECTO-main\Comida\Lasagna.jpg",
-            'category'=>"PLATOS"
+            'category'=>"PLATOS",
+            'quantity'=> 1000
          ]);
          \App\Models\Product::create([
             
             'name'=> "Pizza Cuatro Quesos",
             'price'=> 80000,
             'imagen'=> "PROYECTO-main\Comida\pizza_cuatro_quesos.jpg",
-            'category'=>"PLATOS"
+            'category'=>"PLATOS",
+            'quantity'=> 1000
          ]);
          \App\Models\Product::create([
             
             'name'=> "Vino tinto italiano",
             'price'=> 20000,
             'imagen'=> "PROYECTO-main\Comida\VINO-TINTO-ITALIANO.png",
-            'category'=>"BEBIDAS"
+            'category'=>"BEBIDAS",
+            'quantity'=> 1000
          ]);
          \App\Models\Product::create([
             
             'name'=> "Limonada fresca",
             'price'=> 14000,
             'imagen'=> "PROYECTO-main\Comida\Limonada.jpg",
-            'category'=>"BEBIDAS"
+            'category'=>"BEBIDAS",
+            'quantity'=> 1000
          ]);
          \App\Models\Product::create([
             
             'name'=> "Vino blanco",
             'price'=> 20000,
             'imagen'=> "PROYECTO-main\Comida\VINO-BLANCO.png",
-            'category'=>"BEBIDAS"
+            'category'=>"BEBIDAS",
+            'quantity'=> 1000
          ]);
          \App\Models\Product::create([
             
             'name'=> "Sangría",
             'price'=> 23000,
             'imagen'=> "PROYECTO-main\Comida\sangria.jpg",
-            'category'=>"BEBIDAS"
+            'category'=>"BEBIDAS",
+            'quantity'=> 1000
          ]);
          \App\Models\Product::create([
             
             'name'=> "Capuccino",
             'price'=> 12000,
             'imagen'=> "PROYECTO-main\Comida\capuccino.jpg",
-            'category'=>"BEBIDAS"
+            'category'=>"BEBIDAS",
+            'quantity'=> 1000
          ]);
          \App\Models\Product::create([
             
             'name'=> "Tiramisu",
             'price'=> 13000,
             'imagen'=> "PROYECTO-main\Comida\tiramisu.webp",
-            'category'=>"POSTRES"
+            'category'=>"POSTRES",
+            'quantity'=> 1000
          ]);
          \App\Models\Product::create([
             
             'name'=> "Cannoli",
             'price'=> 10000,
             'imagen'=> "PROYECTO-main\Comida\canoli.jpg",
-            'category'=>"POSTRES"
+            'category'=>"POSTRES",
+            'quantity'=> 1000
          ]);
          \App\Models\Product::create([
             
             'name'=> "Panna cotta",
             'price'=> 15000,
             'imagen'=> "PROYECTO-main\Comida\Panna_cotta.jpg",
-            'category'=>"POSTRES"
+            'category'=>"POSTRES",
+            'quantity'=> 1000
          ]);
          \App\Models\Product::create([
             
             'name'=> "Gelato",
             'price'=> 8000,
             'imagen'=> "PROYECTO-main\Comida\Gelato.jpg",
-            'category'=>"POSTRES"
+            'category'=>"POSTRES",
+            'quantity'=> 1000
          ]);
     }
    }
